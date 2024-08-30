093d737df651f5451f03f554ae767fb2cb0b8ce4
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-08-29 23:28:28 -0300

    :sparkles: feat: athletes model

---
+++ app/athlete/model.py
@@ -0,0 +1,8 @@
+from app import db
+
+
+class Athlete(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'), nullable=False)
+    sport_id = db.Column(db.Integer, db.ForeignKey('sport.id'), nullable=False)
