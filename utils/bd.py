f048364fb5c52748ccc95e018521fcded9cf6851
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-08-29 23:27:50 -0300

    :wrench: chore: db configuration

---
+++ utils/bd.py
@@ -0,0 +1,24 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+print("Conectando...")
+try:
+    conn = mysql.connector.connect(
+        host='127.0.0.1',
+        user='root',
+        password='admin'
+    )
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print('Existe algo errado no nome de usuário ou senha')
+    else:
+        print(err)
+
+cursor = conn.cursor()
+
+cursor.execute("DROP DATABASE IF EXISTS `olympic`;")
+
+cursor.execute("CREATE DATABASE `olympic`;")
+
+cursor.execute("USE `olympic`;")
+
